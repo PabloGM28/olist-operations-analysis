033f2c21f82469cdfdba7572eef69a771b67043b
PabloGM28 <164219994+PabloGM28@users.noreply.github.com>
2026-06-08 15:54:03 +0200

    Update 02_delivery_performance.sql

--- sql/02_delivery_performance.sql
+++ sql/02_delivery_performance.sql
@@ -15,3 +15,21 @@
 
 -- Result obtained during the data understanding phase:
 -- Valid delivered orders = 96,476
+-- ============================================================
+-- KPI 2 & KPI 3: On-Time Delivery Rate
+-- ============================================================
+
+-- Definitions:
+-- On-Time Order:
+-- order_delivered_customer_date <= order_estimated_delivery_date
+--
+-- Delayed Order:
+-- order_delivered_customer_date > order_estimated_delivery_date
+
+SELECT
+  COUNT(*) AS total_deliveries,
+  COUNT(CASE WHEN order_delivered_customer_date <= order_estimated_delivery_date THEN 1 END) AS on_time_deliveries,
+  COUNT(CASE WHEN order_delivered_customer_date > order_estimated_delivery_date THEN 1 END) AS delayed_deliveries,
+  SAFE_DIVIDE (COUNT(CASE WHEN order_delivered_customer_date <= order_estimated_delivery_date THEN 1 END),COUNT(*)) AS on_time_delivery_rate,
+  SAFE_DIVIDE (COUNT(CASE WHEN order_delivered_customer_date > order_estimated_delivery_date THEN 1 END),COUNT(*)) AS delayed_deliveries_rate  
+FROM `olist-operations-analytics.olist_analysis.clean_orders`
