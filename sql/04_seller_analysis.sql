4584c59b7871fbe985e8b5ca3c68ddc165fc1eac
PabloGM28 <164219994+PabloGM28@users.noreply.github.com>
2026-06-10 12:50:09 +0200

    Update 04_seller_analysis.sql

--- sql/04_seller_analysis.sql
+++ sql/04_seller_analysis.sql
@@ -12,5 +12,19 @@ ON o.order_id=s.order_id
 GROUP BY s.seller_id
 ORDER BY total_orders_per_seller DESC
 -- ============================================================
--- KPI 2: Delayed Delivery Rate by Seller
+-- KPI 2 & KPI 3: On-Time and Delayed Delivery Rate by Seller
 -- ============================================================
+SELECT  
+  s.seller_id AS seller,
+  COUNT(o.order_id) AS total_orders_per_seller,
+  COUNT(CASE WHEN o.order_delivered_customer_date <= o.order_estimated_delivery_date THEN 1 END) AS on_time_deliveries,
+  COUNT(CASE WHEN o.order_delivered_customer_date > o.order_estimated_delivery_date THEN 1 END) AS delayed_deliveries,
+  SAFE_DIVIDE(COUNT(CASE WHEN o.order_delivered_customer_date <= o.order_estimated_delivery_date THEN 1 END),COUNT(o.order_id)) AS on_time_delivery_rate,
+  SAFE_DIVIDE(COUNT(CASE WHEN o.order_delivered_customer_date > o.order_estimated_delivery_date THEN 1 END),COUNT(o.order_id)) AS delayed_delivery_rate
+  
+FROM `olist-operations-analytics.olist_analysis.clean_orders` AS o
+INNER JOIN `olist-operations-analytics.olist_analysis.clean_order_items` AS s
+ON o.order_id=s.order_id
+
+GROUP BY s.seller_id
+ORDER BY total_orders_per_seller DESC
