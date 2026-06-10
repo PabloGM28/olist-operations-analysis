4c5c67471080a96c0fac232e12f027201bcb53dc
PabloGM28 <164219994+PabloGM28@users.noreply.github.com>
2026-06-10 13:00:08 +0200

    Update 04_seller_analysis.sql

--- sql/04_seller_analysis.sql
+++ sql/04_seller_analysis.sql
@@ -29,6 +29,21 @@ ON o.order_id=s.order_id
 GROUP BY s.seller_id
 ORDER BY total_orders_per_seller DESC
 -- ============================================================
+-- KPI 4: Average Delivey Time by Seller
+-- ============================================================
+  SELECT  
+
+  s.seller_id AS seller,
+  COUNT(o.order_id) AS total_orders,
+  AVG(DATE_DIFF(DATE(o.order_delivered_customer_date),DATE(o.order_purchase_timestamp),DAY)) AS average_delivery_time
+  
+FROM `olist-operations-analytics.olist_analysis.clean_orders` AS o
+INNER JOIN `olist-operations-analytics.olist_analysis.clean_order_items` AS s
+ON o.order_id=s.order_id
+
+GROUP BY s.seller_id
+ORDER BY total_orders DESC
+-- ============================================================
 -- KPI 6: Delayed Orders by Seller
 -- ============================================================
 SELECT  
