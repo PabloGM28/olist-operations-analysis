d1dbbcc669b1fd230e52655492f4a1996f1d10e5
PabloGM28 <164219994+PabloGM28@users.noreply.github.com>
2026-06-10 11:52:56 +0200

    Update 04_seller_analysis.sql

--- sql/04_seller_analysis.sql
+++ sql/04_seller_analysis.sql
@@ -1,1 +1,12 @@
+KPI 1 Orders by seller
 
+SELECT  
+  s.seller_id AS seller,
+  COUNT(o.order_id) AS total_orders_per_seller
+  
+FROM `olist-operations-analytics.olist_analysis.clean_orders` AS o
+INNER JOIN `olist-operations-analytics.olist_analysis.clean_order_items` AS s
+ON o.order_id=s.order_id
+
+GROUP BY s.seller_id
+ORDER BY total_orders_per_seller DESC
