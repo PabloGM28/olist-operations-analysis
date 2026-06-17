9835fc3d345a2e249baf8285d4d9889df6989530
PabloGM28 <164219994+PabloGM28@users.noreply.github.com>
2026-06-17 18:38:39 +0200

    Update 06_customer_review.sql

--- sql/06_customer_review.sql
+++ sql/06_customer_review.sql
@@ -43,3 +43,44 @@ GROUP BY delivery_status
 -- ============================================================
 -- KPI 3: Average Review Score by Delay Severity Group
 -- ============================================================
+WITH categorized_deliveries AS (
+
+  SELECT
+    order_id,
+    customer_id,
+    order_status,
+    order_purchase_timestamp,
+    order_approved_at,
+    order_delivered_carrier_date,
+    order_delivered_customer_date,
+    order_estimated_delivery_date,
+    CASE
+    WHEN order_delivered_customer_date<=order_estimated_delivery_date THEN 'ON_TIME'
+    WHEN DATE_DIFF(DATE(order_delivered_customer_date),DATE(order_estimated_delivery_date),DAY) BETWEEN 1 AND 3 THEN '1-3_DAYS'
+    WHEN DATE_DIFF(DATE(order_delivered_customer_date),DATE(order_estimated_delivery_date),DAY) BETWEEN 4 AND 7 THEN '4-7_DAYS'
+    WHEN DATE_DIFF(DATE(order_delivered_customer_date),DATE(order_estimated_delivery_date),DAY) BETWEEN 8 AND 14 THEN '8-14_DAYS'
+    ELSE '>14_DAYS'
+END AS delay_group
+    FROM `olist-operations-analytics.olist_analysis.clean_orders`
+)
+
+SELECT
+  c.delay_group,
+  AVG(r.review_score) AS average_review_score,
+  COUNT(c.order_id) AS total_orders,
+  SAFE_DIVIDE(COUNT(c.order_id),SUM(COUNT(c.order_id)) OVER())*100 AS percentage_of_total
+FROM categorized_deliveries AS c
+INNER JOIN `olist-operations-analytics.olist_analysis.clean_order_reviews` AS r
+  ON c.order_id=r.order_id
+GROUP BY delay_group
+ORDER BY
+  CASE c.delay_group
+    WHEN 'ON_TIME' THEN 1
+    WHEN '1-3_DAYS' THEN 2
+    WHEN '4-7_DAYS' THEN 3
+    WHEN '8-14_DAYS' THEN 4
+    WHEN '>14_DAYS' THEN 5
+  END;
+-- ============================================================
+-- KPI 4: Review Response Rate by Delivery Status
+-- ============================================================
