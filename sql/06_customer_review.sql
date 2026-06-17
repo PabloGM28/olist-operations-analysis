dad58984601694d2860b5d6b24b4dfe96f744909
PabloGM28 <164219994+PabloGM28@users.noreply.github.com>
2026-06-17 17:48:26 +0200

    Update 06_customer_review.sql

--- sql/06_customer_review.sql
+++ sql/06_customer_review.sql
@@ -11,3 +11,35 @@ INNER JOIN `olist-operations-analytics.olist_analysis.clean_order_reviews` AS r
 -- ============================================================
 -- KPI 2: Review Score Distriburion by Delivery Status
 -- ============================================================
+WITH categorized_deliveries AS (
+
+  SELECT
+    order_id,
+    customer_id,
+    order_status,
+    order_purchase_timestamp,
+    order_approved_at,
+    order_delivered_carrier_date,
+    order_delivered_customer_date,
+    order_estimated_delivery_date,
+    CASE
+      WHEN order_delivered_customer_date<=order_estimated_delivery_date THEN "ON_TIME"
+      ELSE "DELAYED"
+    END AS delivery_status
+    FROM `olist-operations-analytics.olist_analysis.clean_orders`
+)
+
+SELECT
+  c.delivery_status,
+  ROUND(SAFE_DIVIDE(COUNT(CASE WHEN r.review_score=1 THEN 1 END),COUNT(*))*100,2) AS one_star_reviews_percentage,
+  ROUND(SAFE_DIVIDE(COUNT(CASE WHEN r.review_score=2 THEN 1 END),COUNT(*))*100,2) AS two_star_reviews_percentage,
+  ROUND(SAFE_DIVIDE(COUNT(CASE WHEN r.review_score=3 THEN 1 END),COUNT(*))*100,2) AS three_star_reviews_percentage,
+  ROUND(SAFE_DIVIDE(COUNT(CASE WHEN r.review_score=4 THEN 1 END),COUNT(*))*100,2) AS four_star_reviews_percentage,
+  ROUND(SAFE_DIVIDE(COUNT(CASE WHEN r.review_score=5 THEN 1 END),COUNT(*))*100,2) AS five_star_reviews_percentage
+FROM `categorized_deliveries` AS c
+INNER JOIN `olist-operations-analytics.olist_analysis.clean_order_reviews` AS r
+  ON c.order_id=r.order_id
+GROUP BY delivery_status
+-- ============================================================
+-- KPI 3: Average Review Score by Delay Severity Group
+-- ============================================================
