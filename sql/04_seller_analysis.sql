b532f8f7966ad764d435ab8babe1b03fc35524f2
PabloGM28 <164219994+PabloGM28@users.noreply.github.com>
2026-06-18 18:22:06 +0200

    Update 04_seller_analysis.sql

--- sql/04_seller_analysis.sql
+++ sql/04_seller_analysis.sql
@@ -24,7 +24,8 @@
 
 SELECT  
   s.seller_id AS seller,
-  COUNT(o.order_id) AS total_orders_per_seller
+  COUNT(o.order_id) AS total_orders_per_seller,
+  SAFE_DIVIDE(COUNT(o.order_id),SUM(COUNT(o.order_id)) OVER()) AS percentage_of_total
   
 FROM `olist-operations-analytics.olist_analysis.clean_orders` AS o
 INNER JOIN `olist-operations-analytics.olist_analysis.clean_order_items` AS s
@@ -49,6 +50,7 @@ ORDER BY total_orders_per_seller DESC
 SELECT  
   s.seller_id AS seller,
   COUNT(o.order_id) AS total_orders_per_seller,
+  SAFE_DIVIDE(COUNT(o.order_id),SUM(COUNT(o.order_id)) OVER()) AS percentage_of_total,
   COUNT(CASE WHEN o.order_delivered_customer_date <= o.order_estimated_delivery_date THEN 1 END) AS on_time_deliveries,
   COUNT(CASE WHEN o.order_delivered_customer_date > o.order_estimated_delivery_date THEN 1 END) AS delayed_deliveries,
   SAFE_DIVIDE(COUNT(CASE WHEN o.order_delivered_customer_date <= o.order_estimated_delivery_date THEN 1 END),COUNT(o.order_id)) AS on_time_delivery_rate,
@@ -60,7 +62,6 @@ ON o.order_id=s.order_id
 
 GROUP BY s.seller_id
 ORDER BY total_orders_per_seller DESC
-
 
 -- ============================================================
 -- KPI 4: Average Delivery Time by Seller
@@ -78,6 +79,7 @@ SELECT
 
   s.seller_id AS seller,
   COUNT(o.order_id) AS total_orders,
+  SAFE_DIVIDE(COUNT(o.order_id),SUM(COUNT(o.order_id)) OVER()) AS percentage_of_total,
   AVG(DATE_DIFF(DATE(o.order_delivered_customer_date),DATE(o.order_purchase_timestamp),DAY)) AS average_delivery_time_days
   
 FROM `olist-operations-analytics.olist_analysis.clean_orders` AS o
@@ -104,6 +106,7 @@ SELECT
 
   s.seller_id AS seller,
   COUNT(o.order_id) AS total_orders,
+  SAFE_DIVIDE(COUNT(o.order_id),SUM(COUNT(o.order_id)) OVER()) AS percentage_of_total,
   AVG(CASE WHEN DATE(o.order_delivered_customer_date) > DATE (o.order_estimated_delivery_date) THEN DATE_DIFF(DATE(o.order_delivered_customer_date),DATE(o.order_estimated_delivery_date),DAY) END) AS average_delay_time_days
 
 FROM `olist-operations-analytics.olist_analysis.clean_orders` AS o
@@ -128,6 +131,7 @@ ORDER BY total_orders DESC
 SELECT  
   s.seller_id AS seller,
   COUNT(o.order_id) AS total_orders_per_seller,
+  SAFE_DIVIDE(COUNT(o.order_id),SUM(COUNT(o.order_id)) OVER()) AS percentage_of_total,
   COUNT(CASE WHEN o.order_delivered_customer_date <= o.order_estimated_delivery_date THEN 1 END) AS on_time_deliveries,
   COUNT(CASE WHEN o.order_delivered_customer_date > o.order_estimated_delivery_date THEN 1 END) AS delayed_deliveries
 FROM `olist-operations-analytics.olist_analysis.clean_orders` AS o
