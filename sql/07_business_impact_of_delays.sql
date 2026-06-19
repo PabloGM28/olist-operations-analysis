41f6596527998ea524786e14b41edf97ace5e195
PabloGM28 <164219994+PabloGM28@users.noreply.github.com>
2026-06-19 17:15:12 +0200

    Update 07_business_impact_of_delays.sql

--- sql/07_business_impact_of_delays.sql
+++ sql/07_business_impact_of_delays.sql
@@ -72,3 +72,88 @@ SELECT
 FROM customers_delay_experience
 
 GROUP BY customer_delay_group;
+-- ============================================================
+-- KPI 2: Repurchase Rate After Experiencing a Delay
+-- ============================================================
+
+-- Definition:
+-- Customers who experienced at least one delayed order are
+-- classified into two groups:
+--   - RETURNED_AFTER_DELAY: customers whose last order was
+--     placed after their first delayed order.
+--   - DID_NOT_RETURN_AFTER_DELAY: customers who never placed
+--     another order after experiencing their first delay.
+--
+-- Purpose:
+-- Measure whether customers continue purchasing after a poor
+-- delivery experience.
+
+WITH orders_categorized_deliveries AS (
+  SELECT
+    order_id,
+    o.customer_id,
+    c.customer_unique_id,
+    order_status,
+    order_purchase_timestamp,
+    order_approved_at,
+    order_delivered_carrier_date,
+    order_delivered_customer_date,
+    order_estimated_delivery_date,
+    CASE
+      WHEN order_delivered_customer_date<=order_estimated_delivery_date THEN "ON_TIME"
+      ELSE "DELAYED"
+    END AS delivery_status
+    FROM `olist-operations-analytics.olist_analysis.clean_orders` AS o
+    INNER JOIN `olist-operations-analytics.olist_analysis.clean_customers` AS c
+      ON o.customer_id=c.customer_id
+),
+
+customer_dates AS (
+  SELECT
+    customer_unique_id,
+    MIN(
+      CASE
+        WHEN delivery_status = "DELAYED"
+        THEN order_purchase_timestamp
+      END
+    ) AS first_delayed_order_date,
+
+    MAX(order_purchase_timestamp) AS last_order_date
+
+  FROM orders_categorized_deliveries
+
+  GROUP BY customer_unique_id
+),
+
+repurchase_after_delay AS (
+  SELECT
+    customer_unique_id,
+
+    CASE
+      WHEN last_order_date > first_delayed_order_date
+      THEN "RETURNED_AFTER_DELAY"
+
+      ELSE "DID_NOT_RETURN_AFTER_DELAY"
+
+    END AS repurchase_status
+
+  FROM customer_dates
+
+  WHERE first_delayed_order_date IS NOT NULL
+)
+
+SELECT
+  repurchase_status,
+  COUNT(customer_unique_id) AS total_customers,
+
+  ROUND(
+    SAFE_DIVIDE(
+      COUNT(customer_unique_id),
+      SUM(COUNT(customer_unique_id)) OVER()
+    ) * 100,
+    2
+  ) AS percentage_of_delayed_customers
+
+FROM repurchase_after_delay
+
+GROUP BY repurchase_status;
